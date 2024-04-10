08da522e6c6ea78f678e95f9eaead6c0fc214012
Sam Low <sammmlow@stanford.edu>
2024-04-10 00:11:00 -0700

    Added density calcs for unmodelled arm assembly

--- rom/density_calcs.py
+++ rom/density_calcs.py
@@ -10,21 +10,23 @@ rho_ti = 4.43 # g/cm^3, Ti-6Al-4V
 rho_si = 2.33 # g/cm^3, Silicon
 rho_xe = 0.5 # g/cm^3, Xenon
 rho_empty = 0.0 # g/cm^3, Empty space
+rho_im7 = 1.78 # g/cm^3, graphite-resin epoxy
 
 units = "g/cm^3"
 
-rhos = [rho_ti, rho_si, rho_xe, rho_empty]
+rhos = [rho_ti, rho_si, rho_xe, rho_empty, rho_im7]
 
 # VOLUMETRIC FRACTIONS
-hub_fracs = [0.1, 0.05, 0.0, 0.85]
-solar_panel_fracs = [0.2, 0.2, 0.0, 0.6]
-fuel_tank_fracs = [1.0, 0.0, 0.0, 0.0]
-fuel_in_tank_fracs = [0.0, 0.0, 1.0, 0.0]
-antenna_fracs = [0.15, 0.1, 0.0, 0.75]
-docking_port_fracs = [0.05, 0.05, 0.0, 0.9]
+hub_fracs = [0.1, 0.05, 0.0, 0.85, 0.0]
+solar_panel_fracs = [0.2, 0.2, 0.0, 0.6, 0.0]
+fuel_tank_fracs = [1.0, 0.0, 0.0, 0.0, 0.0]
+fuel_in_tank_fracs = [0.0, 0.0, 1.0, 0.0, 0.0]
+antenna_fracs = [0.15, 0.1, 0.0, 0.75, 0.0]
+docking_port_fracs = [0.05, 0.05, 0.0, 0.9, 0.0]
+arm_fracs = [0.0, 0.1, 0.0, 0.8, 0.1]
 
 part_fracs = [hub_fracs, solar_panel_fracs, fuel_tank_fracs,
-              fuel_in_tank_fracs, antenna_fracs, docking_port_fracs]
+              fuel_in_tank_fracs, antenna_fracs, docking_port_fracs, arm_fracs]
 
 # Check that the fractions sum to 1
 for fracs in part_fracs:
@@ -37,7 +39,7 @@ densities = [density(rhos, fracs) for fracs in part_fracs]
 
 # Print the results
 names = ["Hub", "Solar Panel", "Fuel Tank",
-         "Fuel in Tank", "Antenna", "Docking Port"]
+         "Fuel in Tank", "Antenna", "Docking Port", "Arm Assembly"]
 for name_part, rho_part in zip(names, densities):
     print(f"{name_part:12} density: {rho_part:.2f} {units}")
 
