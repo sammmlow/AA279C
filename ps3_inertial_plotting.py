1279973fef4efa7302a0e2926ba0bd95110a1dc2
Daniel Neamati <danineamati@gmail.com>
2024-04-24 23:05:17 -0700

    Added Herpolhode Plot

--- ps3_inertial_plotting.py
+++ ps3_inertial_plotting.py
@@ -257,76 +257,148 @@ for sat_name, sat_ang_vel, sat_dcm in zip(["MRP", "QTR"],
         inertia_inertial_frame = dcm_curr.T @ initial_inertia @ dcm_curr
         # inertia_inertial_frame = dcm_curr @ initial_inertia @ dcm_curr.T
 
-        angular_momentum_body = inertia_inertial_frame @ omega_inertial_curr
-        angular_momentum_history.append(angular_momentum_body)
+        angular_momentum_inertial = inertia_inertial_frame @ omega_inertial_curr
+        angular_momentum_history.append(angular_momentum_inertial)
+
+        # print(np.dot(angular_momentum_inertial, omega_inertial_curr))
     
     # Store the history of the angular velocities
     angular_momentum_history_per_sat.append(angular_momentum_history)
 
 # Now plot the angular momentum in the inertial frame per satellite
 
-for sat_name, sat_ang_mom in zip(["MRP", "QTR"], angular_momentum_history_per_sat):
-    fig = plt.figure()
-    ax = plt.axes(projection='3d')
+if True:
+    for sat_name, sat_ang_mom in zip(["MRP", "QTR"], angular_momentum_history_per_sat):
+        fig = plt.figure()
+        ax = plt.axes(projection='3d')
 
-    # Set the viewpoint
-    ax.view_init(elev=viewpoint["elev"], azim=viewpoint["azim"])
+        # Set the viewpoint
+        ax.view_init(elev=viewpoint["elev"], azim=viewpoint["azim"])
 
-    # Stack the history of the angular velocities as np array (time, 3)
-    angular_momentum_history = np.array(sat_ang_mom)
-    assert angular_momentum_history.shape[1] == 3
+        # Stack the history of the angular velocities as np array (time, 3)
+        angular_momentum_history = np.array(sat_ang_mom)
+        assert angular_momentum_history.shape[1] == 3
 
-    # Just plot the tip of the angular momentum vector
-    # Plot by alpha which requires plotting each point separately
-    for (lxi, lyi, lzi), alpha in zip(sat_ang_mom, alpha_linear_scaled):
-        ax.scatter3D(lxi, lyi, lzi, color="k", alpha=alpha)
+        # Just plot the tip of the angular momentum vector
+        # Plot by alpha which requires plotting each point separately
+        for (lxi, lyi, lzi), alpha in zip(sat_ang_mom, alpha_linear_scaled):
+            ax.scatter3D(lxi, lyi, lzi, color="k", alpha=alpha)
 
-    ax.set_xlabel("x")
-    ax.set_ylabel("y")
-    ax.set_zlabel("z")
-    
-    ax.axis("equal")
-    
-    # Use more human names for the title
-    if sat_name == "MRP":
-        title_name = "Angular Momentum when using the Modified Rodrigues Parameters"
-    else:
-        title_name = "Angular Momentum when using the Quaternions"
+        ax.set_xlabel("x")
+        ax.set_ylabel("y")
+        ax.set_zlabel("z")
+        
+        ax.axis("equal")
+        
+        # Use more human names for the title
+        if sat_name == "MRP":
+            title_name = "Angular Momentum when using the Modified Rodrigues Parameters"
+        else:
+            title_name = "Angular Momentum when using the Quaternions"
 
-    plt.title(title_name)
-    plt.show()
+        plt.title(title_name)
+        plt.show()
 
-    # Save the figure
-    fig.savefig(f"ps3_data/{sat_name}_angular_momentum_body_inertial_max{max_i}_skip{num_skip}.png")
+        # Save the figure
+        fig.savefig(f"ps3_data/{sat_name}_angular_momentum_body_inertial_max{max_i}_skip{num_skip}.png")
 
 
 # Check the norm of the angular momentum, it should be constant
 
-fig = plt.figure()
+if True:
+    fig = plt.figure()
 
-print("\nPlotting norm of the angular momentum\n")
+    print("\nPlotting norm of the angular momentum\n")
 
-for sat_name, sat_ang_mom, sat_times in zip(["MRP", "QTR"], 
+    for sat_name, sat_ang_mom, sat_times in zip(["MRP", "QTR"], 
+                                                angular_momentum_history_per_sat,
+                                                times_history_per_sat):
+        print(f"Length of angular momentum history for {sat_name} is {len(sat_ang_mom)}")
+        angular_momentum_norm = np.linalg.norm(np.array(sat_ang_mom), axis=1)
+
+        # Plot the norm of the angular momentum    
+        plt.plot(sat_times, angular_momentum_norm, label=sat_name,
+                linestyle="--" if sat_name == "MRP" else ":")
+        
+        # Plot the x component of the angular momentum to sanity check
+        # plt.plot(sat_times, np.array(sat_ang_mom)[:, 0], label=f"{sat_name} x",
+        #          linestyle="--" if sat_name == "MRP" else ":")
+
+    print("Finished for loop\n")
+
+    plt.xlabel("Time [s]")
+    plt.ylabel("Angular Momentum Norm [kg m^2/s]")
+    plt.legend()
+    plt.grid()
+    plt.show()
+
+    # Save the figure
+    fig.savefig(f"ps3_data/angular_momentum_norm_max{max_i}_skip{num_skip}.png")
+
+
+# Plot four subplots with the angular momentum components and norm for each satellite
+fig, axs = plt.subplots(2, 2)
+
+for sat_name, sat_ang_mom, sat_times in zip(["MRP", "QTR"],
                                             angular_momentum_history_per_sat,
                                             times_history_per_sat):
-    print(f"Length of angular momentum history for {sat_name} is {len(sat_ang_mom)}")
-    angular_momentum_norm = np.linalg.norm(np.array(sat_ang_mom), axis=1)
+    linestyle_curr = "--" if sat_name == "MRP" else ":"
 
-    # Plot the norm of the angular momentum    
-    plt.plot(sat_times, angular_momentum_norm, label=sat_name,
-             linestyle="--" if sat_name == "MRP" else ":")
+    axs[0, 0].plot(sat_times, np.array(sat_ang_mom)[:, 0], label=f"{sat_name} x",
+                linestyle=linestyle_curr)
+    axs[0, 1].plot(sat_times, np.array(sat_ang_mom)[:, 1], label=f"{sat_name} y",
+                linestyle=linestyle_curr)
+    axs[1, 0].plot(sat_times, np.array(sat_ang_mom)[:, 2], label=f"{sat_name} z",
+                linestyle=linestyle_curr)
+    axs[1, 1].plot(sat_times, np.linalg.norm(np.array(sat_ang_mom), axis=1), label=f"{sat_name} norm",
+                linestyle=linestyle_curr)
+
+# Set the labels in a loop
+for i, (ax, ax_param) in enumerate(zip(axs.flat, ["x", "y", "z", "norm"])):
+    ax.set_xlabel("Time [s]")
+    ax.set_ylabel(f"L {ax_param} [kg m^2/s]")
+    ax.legend()
+    ax.grid()
+
+plt.tight_layout()
+plt.show()
+
+# Save the figure
+fig.savefig(f"ps3_data/angular_momentum_components_max{max_i}_skip{num_skip}.png")
+
+# Now that we have the angular momentum in the inertial frame, we can calculate the
+# plane of the herpolhode, and plot it with the angular velocity in the inertial frame
+
+# Do this per satellite.
+
+fig = plt.figure()
+for sat_name, sat_ang_vel, sat_ang_mom, sat_times in zip(["MRP", "QTR"], 
+                                              omega_inertial_history_per_sat,
+                                              angular_momentum_history_per_sat,
+                                              times_history_per_sat):
+
+    # Stack the history of the angular velocities as np array (time, 3)
+    omega_inertial_history = np.array(sat_ang_vel)
+    angular_momentum_inertial_history = np.array(sat_ang_mom)
+
+    # Calculate the plane of the herpolhode
+    # Get the constant from the first time step
+    herpolhode_const = np.dot(angular_momentum_inertial_history[0, :], omega_inertial_history[0, :])
+    print(f"Herpolhode constant for {sat_name} is {herpolhode_const}")
+
+    # Compare to principal frame
+    herpolhode_const_principal = np.dot(initial_angular_momentum, initial_omega)
+    print(f"Herpolhode constant for principal frame is {herpolhode_const_principal}")
+
+    herpolhode_by_time = np.sum(angular_momentum_inertial_history * omega_inertial_history, axis=1)
+    plt.plot(sat_times, herpolhode_by_time, label=f"{sat_name} herpolhode",
+                linestyle="--" if sat_name == "MRP" else ":")
     
-    # Plot the x component of the angular momentum to sanity check
-    # plt.plot(sat_times, np.array(sat_ang_mom)[:, 0], label=f"{sat_name} x",
-    #          linestyle="--" if sat_name == "MRP" else ":")
-
-print("Finished for loop\n")
-
 plt.xlabel("Time [s]")
-plt.ylabel("Angular Momentum Norm [kg m^2/s]")
+plt.ylabel("Herpolhode Constant (L dot omega) [kg m^2/s^2]")
 plt.legend()
 plt.grid()
 plt.show()
 
 # Save the figure
-fig.savefig(f"ps3_data/angular_momentum_norm_max{max_i}_skip{num_skip}.png")
+fig.savefig(f"ps3_data/herpolhode_constant_max{max_i}_skip{num_skip}.png")
